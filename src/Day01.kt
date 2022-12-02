562b13df5c084a94ab30b016ebcd43003bd754c0
Sushovan Chakraborty <sushovan86@gmail.com>
2022-12-02 22:12:48 +0000

    Day 01 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,46 @@
+import java.io.File
+import java.util.*
+
+val EMPTY_LINE_SEPARATOR = System.lineSeparator().repeat(2)
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun String.splitByEmptyLine() = this.split(EMPTY_LINE_SEPARATOR)
+
+    fun topElves(size: Int, input: String): List<Int> {
+
+        val priorityQueue = PriorityQueue<Int>()
+
+        input.splitByEmptyLine()
+            .map {
+                it.lines().sumOf(String::toInt)
+            }
+            .forEach {
+
+                priorityQueue.add(it)
+                if (priorityQueue.size > size) {
+                    priorityQueue.poll()
+                }
+            }
+
+        return priorityQueue.toList()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        return topElves(1, input).sum()
     }
+
+    fun part2(input: String): Int {
+        return topElves(3, input).sum()
+    }
+
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = File("resource/Day01_test").readText()
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = File("resource/Day01").readText()
     println(part1(input))
     println(part2(input))
 }
